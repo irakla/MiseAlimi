06bc950327ad869e0a0966011ce296161b1611d1
paad112233@naver.com <paad112233@naver.com>
2019-05-07 07:58:29 +0900

    권한 관련 구현

---
+++ app/src/main/java/com/example/misealimi/PermissionManager.kt
@@ -0,0 +1,47 @@
+package com.example.misealimi
+
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+object PermissionManager{
+
+
+    fun isExist_deniedPermission(context: Context, permissions: Array<out String>) : Boolean
+            = deniedPermListOf(context, permissions).isNotEmpty()
+
+    fun deniedPermListOf(context: Context, permissions: Array<out String>): Array<String>
+        = permissionForGPS.filter {
+            PackageManager.PERMISSION_GRANTED !=
+            ContextCompat.checkSelfPermission(context, it)
+        }.toTypedArray()
+
+    fun showRequest(activity: Activity, permissions: Array<out String>, permissionCode: Int
+                    , forFeature: String, permNameForUser: String)
+    {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(activity)
+
+        builder.setMessage(forFeature + "기능을 위해 " + permNameForUser + "권한이 필요합니다.")
+        builder.setPositiveButton("예",
+            object: DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, id: Int)
+                    = ActivityCompat.requestPermissions(activity, permissions, PERMISSIONCODE_GPS)
+            })
+        builder.setNegativeButton("아니오(종료)",
+            object: DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int){
+                    activity.finishActivity(0)
+                    activity.finish()
+                    System.runFinalization()
+                    android.os.Process.killProcess(android.os.Process.myPid() );
+                }
+            })
+
+        builder.show()
+    }
+}
