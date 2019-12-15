ed9589f6130328680f0c33347f28b86a272642b8
paad112233@naver.com <paad112233@naver.com>
2019-12-16 03:15:28 +0900

    통계 관련 기능 추가 UI변경코드 UI스레드에서 실행되도록 수정

--- app/src/main/java/com/example/myapplication/ui/StatisticFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/StatisticFragment.kt
@@ -9,6 +9,7 @@ import com.example.myapplication.DownloaderForStatistics
 import com.example.myapplication.R
 import com.example.myapplication.StatsMap
 import kotlinx.android.synthetic.main.fragment_statistic.*
+import org.jetbrains.anko.runOnUiThread
 
 class StatisticFragment : Fragment(){
     override fun onCreateView(
@@ -20,18 +21,24 @@ class StatisticFragment : Fragment(){
 
         context?.let { context ->
             DownloaderForStatistics(context) { statsMap ->
-                textValueMyGreatestPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_GREATEST_PM10])
-                textValueMyGreatestPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_GREATEST_PM2_5])
-                textValueMyLeastPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_LEAST_PM10])
-                textValueMyLeastPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_LEAST_PM2_5])
-                textValueUserAveragePM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_AVERAGE_PM10])
-                textValueUserAveragePM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_AVERAGE_PM2_5])
-                textValueMaxInspirationPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_GREATEST_PM10])
-                textValueMaxInspirationPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_GREATEST_PM2_5])
+                context.runOnUiThread {
+                    setFineDustValue(statsMap)
+                }
             }.execute()
 
         }
         return nowView
+    }
+
+    private fun setFineDustValue(statsMap: StatsMap) {
+        textValueMyGreatestPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_GREATEST_PM10])
+        textValueMyGreatestPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_GREATEST_PM2_5])
+        textValueMyLeastPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_LEAST_PM10])
+        textValueMyLeastPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_PRIVATE_LEAST_PM2_5])
+        textValueUserAveragePM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_AVERAGE_PM10])
+        textValueUserAveragePM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_AVERAGE_PM2_5])
+        textValueMaxInspirationPM10.text = String.format("미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_GREATEST_PM10])
+        textValueMaxInspirationPM25.text = String.format("초미세먼지 : %.3f㎍/m³", statsMap[DownloaderForStatistics.STATS_KEY_GREATEST_PM2_5])
     }
 
     override fun onResume() {
